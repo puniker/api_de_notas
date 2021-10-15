b453ed41e13488d4502e20f51196e5cc980641ec
Iker Sastre <iker.sastre@dominion-global.com>
2021-10-15 18:22:55 +0200

    control de sesion en la API

--- api/src/index.js
+++ api/src/index.js
@@ -3,8 +3,18 @@ const express = require('express')
 const cors = require('cors')
 // modulos propios
 const routes = require('@routes')
+const rateLimit = require('express-rate-limit')
 
 const app = express()
+
+app.use(
+    rateLimit({
+        windowMs: 12 * 60 * 60 * 1000, // 12 hour duration in milliseconds
+        max: 10000,
+        message: "You exceeded 10000 requests in 12 hour limit!",
+        headers: true,
+    })
+)
 
 app.get('/', function (request, response) {
     response.send('Bienvenido a la API de Notas. Vas a necesitar una api_key para acceder <a href="">aquí</a>.')
